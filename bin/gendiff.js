338f1fe8e644c1081168e2614ca01d473f63fe24
Viktor Dorokhov <superdva@yandex.ru>
2023-12-18 17:45:08 +0300

    add options and arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -12,6 +12,12 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1');
 
+program
+  .option('-f, --format <type>', 'output format');
+
+program
+  .arguments('<filepath1> <filepath2>');
+
 program.parse();
 
 export default genDiff;
