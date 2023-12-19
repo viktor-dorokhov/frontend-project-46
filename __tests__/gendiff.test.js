dd1e0a476cd4778facbfabecd20aeafd66abf0af
Viktor Dorokhov <superdva@yandex.ru>
2023-12-19 19:52:11 +0300

    move lib functions to diff.js add tests for all functions

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,13 +5,45 @@ import genDiff from '../src/gendiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const dir = `${__dirname}/../__fixtures__/`;
 
-test('gendiff', () => {
-  const dir = `${__dirname}/../__fixtures__/`;
-  expect(genDiff(`${dir}file1.json`, `${dir}file2.json`)).toEqual(`- follow: false
+test('gendiff absolute path success', () => {
+  expect(genDiff(`${dir}file1.json`, `${dir}file2.json`)).toEqual(
+    `- follow: false
   host: hexlet.io
 - proxy: 123.234.53.22
 - timeout: 50
 + timeout: 20
-+ verbose: true`);
++ verbose: true`,
+  );
+});
+
+test('gendiff relative path success', () => {
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(
+    `- follow: false
+  host: hexlet.io
+- proxy: 123.234.53.22
+- timeout: 50
++ timeout: 20
++ verbose: true`,
+  );
+});
+
+test('file1 not found', () => {
+  expect(genDiff(`${dir}file3.json`, `${dir}file2.json`)).toBeNull();
+});
+
+test('file2 not found', () => {
+  expect(genDiff(`${dir}file1.json`, `${dir}file4.json`)).toBeNull();
+});
+
+test('wrong files type', () => {
+  expect(genDiff('file1.json', 'file2.txt')).toBeNull();
+  expect(genDiff('file1.docx', 'file2.json')).toBeNull();
+  expect(genDiff('file1', 'file2')).toBeNull();
+});
+
+test('wrong files content', () => {
+  expect(genDiff(`${dir}file1.json`, `${dir}file_wrong.json`)).toBeNull();
+  expect(genDiff(`${dir}file_wrong.json`, `${dir}file2.json`)).toBeNull();
 });
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,39 +1,5 @@
-import _ from 'lodash';
 import { getExt, getFileContent } from './filesystem.js';
-
-const parseJSON = (data) => {
-  try {
-    const json = JSON.parse(data);
-    return json;
-  } catch (err) {
-    console.error(err);
-    return null;
-  }
-};
-
-const getDiffLine = (key, value, diffChar) => `${diffChar || ' '} ${key}: ${value}`;
-
-const getDiffObjects = (object1, object2) => {
-  const keysObject1 = Object.keys(object1);
-  const keysObject2 = Object.keys(object2);
-  const allObjectKeys = _.sortBy(_.union(keysObject1, keysObject2));
-  const result = allObjectKeys.reduce((acc, key) => {
-    const value1 = object1[key];
-    const value2 = object2[key];
-    if (!keysObject1.includes(key)) {
-      return [...acc, getDiffLine(key, value2, '+')];
-    }
-    if (!keysObject2.includes(key)) {
-      return [...acc, getDiffLine(key, value1, '-')];
-    }
-    if (value1 !== value2) {
-      return [...acc, getDiffLine(key, value1, '-'), getDiffLine(key, value2, '+')];
-    }
-    return [...acc, getDiffLine(key, value1)];
-  }, []).join('\n');
-
-  return result;
-};
+import { parseJSON, getObjectsDiff } from './diff.js';
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const ext1 = getExt(filepath1);
@@ -50,15 +16,15 @@ const genDiff = (filepath1, filepath2, format = 'stylish') => {
   if (!fileData2) {
     return null;
   }
-  const json1 = parseJSON(fileData1);
+  const json1 = parseJSON(fileData1, filepath1);
   if (!json1) {
     return null;
   }
-  const json2 = parseJSON(fileData2);
+  const json2 = parseJSON(fileData2, filepath2);
   if (!json2) {
     return null;
   }
-  const result = getDiffObjects(json1, json2, format);
+  const result = getObjectsDiff(json1, json2, format);
 
   return result;
 };
