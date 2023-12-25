0daf45c5e1afdbf59291637dafe61374535bd0b3
Viktor Dorokhov <superdva@yandex.ru>
2023-12-25 16:54:19 +0300

    removed the binding of parsers to the file structure

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,13 +1,47 @@
-import { getObjectsDiff } from './objects.js';
+import { getExt, getFileContent } from './filesystem.js';
+import getObjectsDiff from './objects.js';
 import formatDiffObject from './formatters/index.js';
-import getDataObject from './parsers/index.js';
+import parseData from './parsers/index.js';
+import getMsg from './message.js';
+
+const extMapping = {
+  '.json': 'json',
+  '.yml': 'yaml',
+  '.yaml': 'yaml',
+};
+
+const getFileType = (filepath) => {
+  const ext = getExt(filepath).toLowerCase();
+  const type = extMapping[ext];
+  if (!type) {
+    console.log(getMsg('fileTypeError', ['json, yaml']));
+    return null;
+  }
+  return type;
+};
+
+const getObjectFromFile = (filepath) => {
+  const type = getFileType(filepath);
+  if (!type) {
+    return null;
+  }
+  const fileData = getFileContent(filepath);
+  if (!fileData) {
+    return null;
+  }
+  const object = parseData(fileData, type);
+  if (!object) {
+    return null;
+  }
+  return object;
+};
 
 const genDiff = (filepath1, filepath2, formatType = 'stylish', inColor = false) => {
-  const object1 = getDataObject(filepath1);
+  const object1 = getObjectFromFile(filepath1);
   if (!object1) {
     return null;
   }
-  const object2 = getDataObject(filepath2);
+  const object2 = getObjectFromFile(filepath2);
   if (!object2) {
     return null;
   }
--- src/parsers/index.js
+++ src/parsers/index.js
@@ -1,26 +1,9 @@
-import { getExt, getFileContent } from '../filesystem.js';
-import getMsg from '../message.js';
 import parseJSON from './json.js';
 import parseYAML from './yaml.js';
 
-const allowedExts = ['.json', '.yml', '.yaml'];
-
-const getDataObject = (filepath) => {
-  const ext = getExt(filepath).toLowerCase();
-  if (!allowedExts.includes(ext)) {
-    console.log(getMsg('fileTypeError', ['json, yaml']));
-    return null;
-  }
-  const fileData = getFileContent(filepath);
-  if (!fileData) {
-    return null;
-  }
-  const type = ext === '.json' ? 'JSON' : 'YAML';
-  const result = type === 'JSON' ? parseJSON(fileData) : parseYAML(fileData);
-  if (result === null) {
-    console.log(getMsg('parseError', [type, filepath]));
-  }
-  return result;
+const parseFns = {
+  json: parseJSON,
+  yaml: parseYAML,
 };
 
-export default getDataObject;
+export default (data, type) => parseFns[type](data);
